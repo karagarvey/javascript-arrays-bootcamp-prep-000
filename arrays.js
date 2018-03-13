22abadbe9ce0bb02e8157129b86558c20cbdd1e1
karagarvey <kgarvey87@gmail.com>
2018-03-13 17:35:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function addElementToBeginningofArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   return array.unshift(element);
-  return array
+  return console.log(array)
 };
 
 function addElementToEndofArray(array, element) {
